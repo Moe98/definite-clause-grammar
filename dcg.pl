77315f99e12f916b4a9b80fc3c6311aff28f8250
Moe98 <30552722+Moe98@users.noreply.github.com>
2021-05-07 17:47:50 +0200

    chore: add KB

---
+++ dcg.pl
@@ -0,0 +1,66 @@
+%%%%%%%%% Nouns %%%%%%%%%
+noun(noun(boy)) --> [boy].
+noun(noun(box)) --> [box].
+noun(noun(room)) --> [room].
+noun(noun(school)) --> [school].
+noun(noun(woman)) --> [woman].
+noun(noun(man)) --> [man].
+noun(noun(envelope)) --> [envelope].
+noun(noun(shed)) --> [shed].
+noun(noun(building)) --> [building].
+noun(noun(tree)) --> [tree].
+noun(noun(girl)) --> [girl].
+noun(noun(students)) --> [students].
+noun(noun(professors)) --> [professors].
+noun(noun(lecturers)) --> [lecturers].
+noun(noun(scientists)) --> [scientists].
+noun(noun(researchers)) --> [researchers].
+%%%%%%%%% Nouns %%%%%%%%%
+
+%%%%%%%%% Verbs %%%%%%%%%
+verb(verb(pushed)) --> [pushed].
+verb(verb(stored)) --> [stored].
+verb(verb(gave)) --> [gave].
+verb(verb(climbed)) --> [climbed].
+verb(verb(watched)) --> [watched].
+verb(verb(admired)) --> [admired].
+verb(verb(appreciated)) --> [appreciated].
+verb(verb(did)) --> [did].
+verb(verb(do)) --> [do].
+%%%%%%%%% Verbs %%%%%%%%%
+
+%%%%%%%%% Adjectives %%%%%%%%%
+adjective(adjective(young)) --> [young].
+adjective(adjective(big)) --> [big].
+adjective(adjective(large)) --> [large].
+adjective(adjective(empty)) --> [empty].
+adjective(adjective(old)) --> [old].
+adjective(adjective(poor)) --> [poor].
+adjective(adjective(white)) --> [white].
+adjective(adjective(brilliant)) --> [brilliant].
+adjective(adjective(talented)) --> [talented].
+adjective(adjective(bright)) --> [bright].
+%%%%%%%%% Adjectives %%%%%%%%%
+
+%%%%%%%%% Adverbs %%%%%%%%%
+adverb(adverb(quickly)) --> [quickly].
+%%%%%%%%% Adverbs %%%%%%%%%
+
+%%%%%%%%% Prepositions %%%%%%%%%
+preposition(preposition(in)) --> [in].
+preposition(preposition(after)) --> [after].
+preposition(preposition(behind)) --> [behind].
+%%%%%%%%% Prepositions %%%%%%%%%
+
+%%%%%%%%% Determiners %%%%%%%%%
+determiner(determiner(the)) --> [the].
+determiner(determiner(a)) --> [a].
+determiner(determiner(every)) --> [every].
+determiner(determiner(some)) --> [some].
+determiner(determiner(many)) --> [many].
+%%%%%%%%% Determiners %%%%%%%%%
+
+%%%%%%%%% Interrogative Pronouns %%%%%%%%%
+pronoun(pronoun(who)) --> [who].
+pronoun(pronoun(what)) --> [what].
+%%%%%%%%% Interrogative Pronouns %%%%%%%%%

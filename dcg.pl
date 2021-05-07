d5c7f7ed575d5f63682ae31a460ba01ab67be35d
Moe98 <30552722+Moe98@users.noreply.github.com>
2021-05-07 20:19:24 +0200

    feat: updated the KB

--- dcg.pl
+++ dcg.pl
@@ -66,6 +66,10 @@ noun(noun(professors)) --> [professors].
 noun(noun(lecturers)) --> [lecturers].
 noun(noun(scientists)) --> [scientists].
 noun(noun(researchers)) --> [researchers].
+noun(noun(university)) --> [university].
+noun(noun(home)) --> [home].
+noun(noun(car)) --> [bike].
+noun(noun(airplane)) --> [airplane].
 %%%%%%%%% Nouns %%%%%%%%%
 
 %%%%%%%%% Verbs %%%%%%%%%
@@ -78,6 +82,18 @@ verb(verb(admired)) --> [admired].
 verb(verb(appreciated)) --> [appreciated].
 verb(verb(did)) --> [did].
 verb(verb(do)) --> [do].
+verb(verb(was)) --> [was].
+verb(verb(broke)) --> [broke].
+verb(verb(blew)) --> [blew].
+verb(verb(chose)) --> [chose].
+verb(verb(came)) --> [came].
+verb(verb(drove)) --> [drove].
+verb(verb(found)) --> [found].
+verb(verb(flew)) --> [flew].
+verb(verb(forbade)) --> [forbade].
+verb(verb(forgot)) --> [forgot].
+verb(verb(met)) --> [met].
+verb(verb(accepted)) --> [accepted].
 %%%%%%%%% Verbs %%%%%%%%%
 
 %%%%%%%%% Adjectives %%%%%%%%%
@@ -91,21 +107,51 @@ adjective(adjective(white)) --> [white].
 adjective(adjective(brilliant)) --> [brilliant].
 adjective(adjective(talented)) --> [talented].
 adjective(adjective(bright)) --> [bright].
+adjective(adjective(brave)) --> [brave].
+adjective(adjective(adorable)) --> [adorable].
+adjective(adjective(pretty)) --> [pretty].
+adjective(adjective(beautiful)) --> [beautiful].
+adjective(adjective(blue)) --> [blue].
+adjective(adjective(charming)) --> [charming].
+adjective(adjective(clever)) --> [clever].
+adjective(adjective(crazy)) --> [crazy].
+adjective(adjective(cute)) --> [cute].
+adjective(adjective(dark)) --> [dark].
+adjective(adjective(concerned)) --> [concerned].
 %%%%%%%%% Adjectives %%%%%%%%%
 
 %%%%%%%%% Adverbs %%%%%%%%%
 adverb(adverb(quickly)) --> [quickly].
+adverb(adverb(slowly)) --> [slowly].
+adverb(adverb(clearly)) --> [clearly].
+adverb(adverb(knowingly)) --> [knowingly].
+adverb(adverb(actually)) --> [actually].
+adverb(adverb(freely)) --> [freely].
+adverb(adverb(mostly)) --> [mostly].
+adverb(adverb(closely)) --> [closely].
+adverb(adverb(madly)) --> [madly].
+adverb(adverb(happily)) --> [happily].
+adverb(adverb(gently)) --> [gently].
 %%%%%%%%% Adverbs %%%%%%%%%
 
 %%%%%%%%% Prepositions %%%%%%%%%
 preposition(preposition(in)) --> [in].
 preposition(preposition(after)) --> [after].
 preposition(preposition(behind)) --> [behind].
+preposition(preposition(between)) --> [between].
+preposition(preposition(among)) --> [among].
+preposition(preposition(amidst)) --> [amidst].
+preposition(preposition(beside)) --> [beside].
+preposition(preposition(above)) --> [above].
+preposition(preposition(over)) --> [over].
+preposition(preposition(under)) --> [under].
+preposition(preposition(below)) --> [below].
 %%%%%%%%% Prepositions %%%%%%%%%
 
 %%%%%%%%% Determiners %%%%%%%%%
 determiner(determiner(the)) --> [the].
 determiner(determiner(a)) --> [a].
+determiner(determiner(an)) --> [an].
 determiner(determiner(every)) --> [every].
 determiner(determiner(some)) --> [some].
 determiner(determiner(many)) --> [many].
@@ -122,5 +168,7 @@ conjunctive(conjunctive(but)) --> [but].
 conjunctive(conjunctive(yet)) --> [yet].
 %%%%%%%%% Conjunctives %%%%%%%%%
 
+%%%%%%%%% Punctuation %%%%%%%%%
 period(.) --> [.].
 question_mark(?) --> [?].
+%%%%%%%%% Punctuation %%%%%%%%%
